ad07151cd6a87ad15156c7ccfda9ae681e88d59c
Charles Hixson <charleshixsn@earthlink.net>
2017-10-07 11:10:47 -0700

    modify footers to link to index

--- includes/footer-1.ml
+++ includes/footer-1.ml
@@ -1,4 +1,5 @@
 <footer>
+<p class="rgt"><a href="#ndx">Index</a></p>
 	The text and images on this page, except for the photo of Robin are published under the <a href="!htmlroot!CC_BY_SA.html">
 	Creative Commons Attribution-ShareAlike 4.0</a> International license,
 	also denoted "CC BY-SA 4.0"
